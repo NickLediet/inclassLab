8d74b7880c5867a511e7f13298a0d750ed805ad2
Lediet <n_lediet@tss-hr420-dm05.fc.ca>
2017-11-29 16:47:37 -0500

    finished implementing promise data grabbing

--- includes/functions.php
+++ includes/functions.php
@@ -8,9 +8,10 @@ $db = "a3_cooperInfo";
 
 // Connection to Database instance
 $db_instance = mysqli_connect($host, $user, $pass, $db);
+mysqli_set_charset($db_instance, 'utf8');
 
 // Echo if Err
-echo !$db_instance? die() : "We are connected!";
+// echo !$db_instance? die() : "We are connected!";
 
 #Query Initialization
 $query = "SELECT * FROM mainmodel";
@@ -19,10 +20,26 @@ $query = "SELECT * FROM mainmodel";
 $result = mysqli_query($db_instance, $query);
 $rows = array();
 
-while($row = mysqli_fetch_assoc($result)) {
-    $rows[] = $row; // Push each row from out resault to this array
-}
+
 
 // Dump to the browser
-var_dump($rows); 
+// var_dump($rows); 
+
+// Check if a car model query has been provided
+if(isset($_GET['carModel'])){
+    $car = $_GET['carModel'];
+    $query = "SELECT * FROM mainmodel WHERE model  = '$car'";
+    
+    $result = mysqli_query($db_instance, $query);
+    $carRes= mysqli_fetch_assoc($result);
+
+    // var_dump($carRes);
+
+    // Setup cors
+    header("Access-Control-Allow-Origin: *");
+    $myJson = json_encode($carRes);
+
+    echo $myJson;
+
+}
 
--- js/Car.js
+++ js/Car.js
@@ -6,18 +6,20 @@ class Car {
      * @param {HTMLElement[img]} imageElement and image element to add a click handler to
      * @param {Renderer} render the mounting spopt for the data swapping
      */
-    constructor(carData, imageElement, renderer) 
+    constructor(id, imageElement, renderer) 
     {
-      // Destructure data for what we want
-      const { modelName, pricing, details } = carData
+    //   // Destructure data for what we want
+    //   const { modelName, pricing, details } = carData
   
-      this.modelName = modelName
-      this.pricing = pricing
-      this.details = details
+    //   this.modelName = modelName
+    //   this.pricing = pricing
+    //   this.details = details
+      this.model = id;
       this.imageElement = imageElement
       this.renderer = renderer;
   
-      // AddEvent Listener
+
+    // AddEvent Listener
       this.imageElement.addEventListener("click", () => this.update())
     }
   
@@ -26,20 +28,34 @@ class Car {
      */
     update() 
     {
-        // Remove any focused elements
-      const activeElements = document.querySelectorAll(".focusMini")
-      activeElements ? activeElements.forEach(el => {
-        el.classList.remove("focusMini")
-        el.classList.add("nonActive")
-      }) : null
+      this.getData()
+        .then(data => {
+          const {modelName, pricing, modelDetails} = data
+          // Remove any focused elements
+          const activeElements = document.querySelectorAll(".focusMini")
+          activeElements ? activeElements.forEach(el => {
+            el.classList.remove("focusMini")
+            el.classList.add("nonActive")
+          }) : null
 
-      this.imageElement.classList.add("focusMini")
+          this.imageElement.classList.add("focusMini")
 
-      this.renderer.render({
-        model : this.modelName,
-        pricing : this.pricing,
-        details : this.details
+          this.renderer.render({
+            model : modelName,
+            pricing : pricing,
+            details : modelDetails
+          })
+        });
+    }
+
+    getData()
+    {
+      return new Promise((resolve) => {
+        fetch(`http://localhost:8888/inclasslab/includes/functions.php?carModel=${this.model}`)
+            .then(data => resolve(data.json()))
+            .catch(err => reject(new Error(err)))
       })
+
     }
     
   }
